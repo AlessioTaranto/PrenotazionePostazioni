0e3af1daa5e3bd7f6f35a56f68fab9aac6389b12
Schyerke <stefanohu926@hotmail.com>
2022-07-04 19:20:27 +0200

    implementazione di IMPOSTAZIONI Controller e Repository

--- prenotazioni-postazioni-api/Repositories/ImpostazioneRepository.cs
+++ prenotazioni-postazioni-api/Repositories/ImpostazioneRepository.cs
@@ -2,17 +2,27 @@
 {
     public class ImpostazioneRepository
     {
-        private static ImpostazioneRepository instance;
-        private ImpostazioneRepository()
+
+        /// <summary>
+        /// Restituisce il valore Impostazione Emergenza situato nella tabella Impostazioni nel database.
+        /// </summary>
+        /// <returns>Valore effettivo dell'impostazione di emergenza. True, o False</returns>
+        public bool FindImpostazioneEmergenza()
         {
+            //TODO si bisogna fare una query al db
+            return false;
         }
-        public static ImpostazioneRepository GetInstance()
+
+
+        /// <summary>
+        /// Aggiorna il campo di Impostazioni Emergenza nel Database con il valore inserito nel primo parametro
+        /// </summary>
+        /// <param name="userValue">Il valore con cui si aggiornera Impostazioni Emergenza</param>
+        /// <returns>Lo stato di Impostazione Emergenza aggiornata</returns>
+        public bool UpdateImpostazioneEmergenza(bool userValue)
         {
-            if (instance == null)
-            {
-                instance = new ImpostazioneRepository();
-            }
-            return instance;
+            //TODO si bisogna fare una query al db per aggiornare il campo IMPOSTAZIONE EMERGENZA con *userValue*
+            return false;
         }
     }
 }
