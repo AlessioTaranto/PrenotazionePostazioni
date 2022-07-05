721d9a4053b9789e74dfcaee558a6f382ad3f74b
Schyerke <stefanohu926@hotmail.com>
2022-07-05 14:44:22 +0200

    voto controller signature

--- prenotazioni-postazioni-api/Controllers/VotoController.cs
+++ prenotazioni-postazioni-api/Controllers/VotoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prenotazioni_postazioni_api.Services;
 
 namespace prenotazioni_postazioni_api.Controllers
 {
@@ -6,9 +7,37 @@ namespace prenotazioni_postazioni_api.Controllers
     [Route("/api/voti")]
     public class VotoController : ControllerBase
     {
-        public IActionResult Index()
+        private VotoService votoService = new VotoService();
+
+
+        /// <summary>
+        /// Serve per ottenere l'elenco di votazioni effettuate da un utente verso gli altri
+        /// </summary>
+        /// <param name="idUtente"></param>
+        /// <returns>
+        /// Restituisce una lista di voti in caso di ricerca con esito positivo
+        /// </returns>
+        [Route("getVotiFromUtente")]
+        public IActionResult GetVotiFromUtente(int idUtente)
         {
-            return View();
+
         }
+
+        /// <summary>
+        /// Serve per ottenere l'elenco di tutte le votazioni che un utente ha ricevuto
+        /// </summary>
+        /// <param name="idUtente"></param>
+        /// <returns>
+        /// Restituisce una lista di voti in caso di ricerca con esito positivo
+        /// </returns>
+        [Route("getVotiToUtente")]
+        public IActionResult GetVotiToUtente(int idUtente)
+        {
+
+        }
+
+
+
     }
+}
 }
