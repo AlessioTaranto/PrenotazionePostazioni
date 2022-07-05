1c89077f4bc19659ae3a446c40b946ade7f3e082
DESKTOP-B8G3449\redeg <redegalli03@gmail.com>
2022-07-05 10:36:21 +0200

    Aggiunti modelli

---
+++ prenotazione-postazioni-libs/Models/Impostazioni.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prenotazione_postazioni_libs.Models
+{
+    internal class Impostazioni
+    {
+
+        private static bool modEmergenza;
+
+        public static bool ModEmergenza { get => modEmergenza; set => modEmergenza = value; }
+    }
+}
---
+++ prenotazione-postazioni-libs/Models/Prenotazione.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prenotazione_postazioni_libs.Models
+{
+    internal class Prenotazione
+    {
+
+        private int idPrenotazioni;
+        private DateOnly date;
+        private int idStanza;
+        private int idUtente;
+
+        public int IdPrenotazioni { get => idPrenotazioni; set => idPrenotazioni = value; }
+        public DateOnly Date { get => date; set => date = value; }
+        public int IdStanza { get => idStanza; set => idStanza = value; }
+        public int IdUtente { get => idUtente; set => idUtente = value; }
+    }
+}
