a5a227be634685d0468a7363868d29362bf26b3a
Schyerke <stefanohu926@hotmail.com>
2022-07-08 19:13:30 +0200

    refactored VotoRepository

--- prenotazioni-postazioni-api/Repositories/VotoRepository.cs
+++ prenotazioni-postazioni-api/Repositories/VotoRepository.cs
@@ -13,15 +13,13 @@ namespace prenotazioni_postazioni_api.Repositories
         /// </summary>
         /// <param name="idUtente">L'id dell'utente</param>
         /// <returns>Lista di voti</returns>
-        
         internal List<Voto> FindAllByIdUtenteFrom(int idUtente)
         {
             string query = $"SELECT idVoti, idUtente, idUtenteVotato, voto FROM Voti WHERE idUtente = {idUtente};";
             _databaseManager.CreateConnectionToDatabase(null, null, true);
-            List<Voto> voti = (List<Voto>)JsonConvert.DeserializeObject(_databaseManager.GetOneResult(query));
+            List<Voto> voti = (List<Voto>)JsonConvert.DeserializeObject(_databaseManager.GetAllResults(query));
             _databaseManager.DeleteConnection();
             return voti;
-
         }
 
         /// <summary>
@@ -33,17 +31,17 @@ namespace prenotazioni_postazioni_api.Repositories
         {
             string query = $"SELECT idVoti, idUtente, idUtenteVotato, voto FROM voti WHERE idUtenteVotato = {idUtente};";
             _databaseManager.CreateConnectionToDatabase(null, null, true);
-            List<Voto> voti = (List<Voto>)JsonConvert.DeserializeObject(_databaseManager.GetOneResult(query));
+            List<Voto> voti = (List<Voto>)JsonConvert.DeserializeObject(_databaseManager.GetAllResults(query));
             _databaseManager.DeleteConnection();
             return voti;
         }
         
-        internal Voto UpdateVoti(Voto voto)
-        {
-            string query = $"SELECT voto FROM Voti WHERE idVoti = {voto.IdVoto};";
-            _databaseManager.CreateConnectionToDatabase(null, null, true);
-            bool votoBool = (bool)JsonConvert.DeserializeObject(_databaseManager.GetOneResult(query));
+        //internal Voto UpdateVoti(Voto voto)
+        //{
+        //    string query = $"SELECT voto FROM Voti WHERE idVoti = {voto.IdVoto};";
+        //    _databaseManager.CreateConnectionToDatabase(null, null, true);
+        //    bool votoBool = (bool)JsonConvert.DeserializeObject(_databaseManager.GetOneResult(query));
 
-        }
+        //}
     }
 }
