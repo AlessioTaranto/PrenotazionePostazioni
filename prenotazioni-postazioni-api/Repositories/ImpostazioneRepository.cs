f86e527d3443c233af0d1fcf33c01a9f886801e6
Schyerke <stefanohu926@hotmail.com>
2022-07-08 08:22:23 +0200

    database exception, FindimpostazioneEmergenza ancora da implementare la query

--- prenotazioni-postazioni-api/Repositories/ImpostazioneRepository.cs
+++ prenotazioni-postazioni-api/Repositories/ImpostazioneRepository.cs
@@ -1,5 +1,6 @@
 ﻿using prenotazione_postazioni_libs.Dto;
 using prenotazione_postazioni_libs.Models;
+using Newtonsoft.Json;
 using prenotazioni_postazioni_api.Repositories.Database;
 namespace prenotazioni_postazioni_api.Repositories
 {
@@ -13,8 +14,10 @@ namespace prenotazioni_postazioni_api.Repositories
         /// <returns>Lo stato dell'Impostazione Emergenza</returns>
         public bool FindImpostazioneEmergenza()
         {
-            
-            return false;
+            string query = "";
+            _databaseManager.CreateConnectionToDatabase(null, null, true);
+            bool result = (bool)JsonConvert.DeserializeObject(_databaseManager.GetOneResult(query));
+            return result;
         }
 
         /// <summary>
