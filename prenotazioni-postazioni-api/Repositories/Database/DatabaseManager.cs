c0cbf79bb243c37e78167b375572333973f79e59
manuel mauro <manuelmauro04@gmail.com>
2022-07-13 13:04:55 +0200

    fix database query GetOneResult, da fixxare isValid delle models

--- prenotazioni-postazioni-api/Repositories/Database/DatabaseManager.cs
+++ prenotazioni-postazioni-api/Repositories/Database/DatabaseManager.cs
@@ -57,10 +57,34 @@ namespace prenotazioni_postazioni_api.Repositories.Database
             {
                 SqlCommand cmd = new SqlCommand(query, conn);
                 conn.Open();
-                string jsonResult = JsonConvert.SerializeObject(cmd.ExecuteScalar());
+                var reader = cmd.ExecuteReader();
+                IEnumerable<Dictionary<string, object>> result = Serialize(reader);
+                string jsonResult = JsonConvert.SerializeObject(result);
                 conn.Close();
+                jsonResult = jsonResult.Replace("[", "").Replace("]", "");
                 return jsonResult;
             }
+        }
+
+        public IEnumerable<Dictionary<string, object>> Serialize(SqlDataReader reader)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+                cols.Add(reader.GetName(i));
+
+            while (reader.Read())
+                results.Add(SerializeRow(cols, reader));
+
+            return results;
+        }
+        private Dictionary<string, object> SerializeRow(IEnumerable<string> cols,
+                                                        SqlDataReader reader)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var col in cols)
+                result.Add(col, reader[col]);
+            return result;
         }
 
         /// <summary>
