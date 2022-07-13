f1dc5c88b35b85f95d1fb895099c74c7c6b44374
DESKTOP-B8G3449\redeg <redegalli03@gmail.com>
2022-07-13 11:08:23 +0200

    Conversione pagine Prenotazioni e Rumorosità in cshtml

---
+++ prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prenotazione_postazioni_mvc.Controllers
+{
+    public class VotazioniController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ prenotazione-postazioni-mvc/wwwroot/js/calendar.js
@@ -0,0 +1,88 @@
+const date = new Date;
+let month = date.getMonth();
+
+function loadCalendar() {
+    date.setDate(1);
+
+    $('#month-year').text(translateMonth(date.getMonth()) + " " + date.getFullYear());
+    month = date.getMonth();
+
+    for (let i = 0; i<7; i++)
+        for (let j = 0; j<6; j++) {
+            $('#'.concat(i).concat('-').concat(j)).text("");
+            $('#'.concat(i).concat('-').concat(j)).css("color", "black");
+        }
+
+    let firstLineLoad = true
+
+    for (let i = 0; i<6; i++) {
+        for (let j = 0; j<7; j++) {
+            if (firstLineLoad === true) {
+                date.setDate(date.getDate()-((date.getDay() === 0 ? +6 : date.getDay()-1)));
+                firstLineLoad = false;
+            }
+            $('#'.concat(j).concat('-').concat(i)).text(date.getDate());
+            if (date.getMonth() !== month)
+                $('#'.concat(j).concat('-').concat(i)).css("color", "#a6a6a6");
+            date.setDate(date.getDate()+1);
+        }
+    }
+
+    checkSelected()
+}
+
+function prevMonth() {
+    date.setMonth(date.getMonth()-2);
+    loadCalendar();
+}
+
+function nextMonth() {
+    date.setMonth(date.getMonth());
+    loadCalendar();
+}
+
+function translateMonth(number) {
+    switch (number) {
+        case 0:
+            return "Gennaio";
+        case 1:
+            return "Febbraio";
+        case 2:
+            return "Marzo";
+        case 3:
+            return "Aprile";
+        case 4:
+            return "Maggio";
+        case 5:
+            return "Giugno";
+        case 6:
+            return "Luglio";
+        case 7:
+            return "Agosto";
+        case 8:
+            return "Settembre";
+        case 9:
+            return "Ottobre";
+        case 10:
+            return "Novembre";
+        case 11:
+            return "Dicembre";
+    }
+    return "";
+}
+
+function daysInMonth (month) {
+    return new Date(1970, month, 0).getDate();
+}
+
+function checkSelected() {
+    if (daySelected.getMonth() !== date.getMonth()) {
+        $('#'.concat(dayIdSelected)).css("color","black");
+        $('#'.concat(dayIdSelected)).css("background-color","transparent");
+        $('#'.concat(dayIdSelected)).css("font-weight","normal");
+    } else {
+        $('#'.concat(dayIdSelected)).css("color","white");
+        $('#'.concat(dayIdSelected)).css("background-color","darkorange");
+        $('#'.concat(dayIdSelected)).css("font-weight","bold");
+    }
+}
