f53a9760924124c4a8069782231792d394aaa5b1
Schyerke <stefanohu926@hotmail.com>
2022-07-14 17:24:31 +0200

    festivita rinominato a Festa (colpa di manuel)

---
+++ prenotazioni-postazioni-api/Controllers/FestaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prenotazioni_postazioni_api.Controllers
+{
+    [ApiController]
+    [Route("/api/festivita")]
+    public class FestaController : ControllerBase
+    {
+        [Route("getByDate")]
+        [HttpGet]
+        public IActionResult GetByDate(DateOnly date)
+        {
+            return Ok();
+        }
+
+        [Route("getAll")]
+
+
+        [Route("getAllByMonth")]
+
+        [Route("addFestivita")]
+    }
+}
