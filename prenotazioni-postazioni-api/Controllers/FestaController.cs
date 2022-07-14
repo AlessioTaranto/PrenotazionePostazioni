3b3e38f09123d4d8a8b222e08f1ba9225e345ce3
Schyerke <stefanohu926@hotmail.com>
2022-07-14 17:30:25 +0200

    GetBydate festa fatto

--- prenotazioni-postazioni-api/Controllers/FestaController.cs
+++ prenotazioni-postazioni-api/Controllers/FestaController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prenotazione_postazioni_libs.Models;
+using prenotazioni_postazioni_api.Exceptions;
+using prenotazioni_postazioni_api.Services;
 
 namespace prenotazioni_postazioni_api.Controllers
 {
@@ -6,11 +9,31 @@ namespace prenotazioni_postazioni_api.Controllers
     [Route("/api/festivita")]
     public class FestaController : ControllerBase
     {
+        private FestaService _festaService = new FestaService();
+
+
         [Route("getByDate")]
         [HttpGet]
         public IActionResult GetByDate(DateOnly date)
         {
-            return Ok();
+            try
+            {
+                Festa festa = _festaService.GetByDate(date);
+                if(festa == null)
+                {
+                    return NotFound("Festa e' null");
+                }
+                return Ok(festa);
+            }
+            catch(PrenotazionePostazioniApiException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
         }
 
         [Route("getAll")]
