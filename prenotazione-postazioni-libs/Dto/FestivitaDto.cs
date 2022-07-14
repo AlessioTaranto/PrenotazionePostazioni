7d2714ee2cd83d0e78421ed284ce6f7151b0a206
Schyerke <stefanohu926@hotmail.com>
2022-07-14 17:13:26 +0200

    festivitas dto

---
+++ prenotazione-postazioni-libs/Dto/FestivitaDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prenotazione_postazioni_libs.Dto
+{
+    public class FestivitaDto
+    {
+        public DateOnly Date { get; set; }
+        public string? Desc { get; set; }
+        
+        public FestivitaDto()
+        {
+
+        }
+        public FestivitaDto(DateOnly date, string? desc)
+        {
+            Date = date;
+            Desc = desc;
+        }
+        public FestivitaDto(DateOnly date)
+        {
+            this.Date = date;
+        }
+    }
+}
