0af02d1b768deef197e908a5704120fd0c65ed23
DESKTOP-B8G3449\redeg <redegalli03@gmail.com>
2022-07-14 17:21:13 +0200

    Progetto MVC

---
+++ prenotazione-postazioni-mvc/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using prenotazione_postazioni_mvc.Models;
+
+namespace prenotazione_postazioni_mvc.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        
+
+        return View();
+    }
+}
