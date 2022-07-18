9b57311aa181abb4a8999a391d57584e57fdbe94
DESKTOP-B8G3449\redeg <redegalli03@gmail.com>
2022-07-18 17:05:14 +0200

    Terminata la pagina postazioni.
    Fixare collapse, aggiungere Lista prenotazioni (API)

---
+++ prenotazione-postazioni-mvc/wwwroot/js/keep-collapse.js
@@ -0,0 +1,18 @@
+﻿$(".collapse").on('show.bs.collapse', function () {
+    var active = $(this).attr('id');
+    $.cookie(active, "1");
+    alert("show");
+});
+
+$(".collapse").on('hide.bs.collapse', function () {
+    var active = $(this).attr('id');
+    alert("hide");
+    $.removeCookie(active);
+});
+
+$(document.ready(function () {
+    var panels = $.cookie(); //get all cookies
+    for (var panel in panels) { //<-- panel is the name of the cookie
+        console.log(panel);
+    }
+}));
