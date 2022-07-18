da266f9bb5d9a69fffa5aa03b2e0fe2d0b346292
Schyerke <stefanohu926@hotmail.com>
2022-07-18 14:23:08 +0200

    aggiunto configurazione log4net

--- prenotazioni-postazioni-api/Program.cs
+++ prenotazioni-postazioni-api/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Log4Net.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add logging Log4Net
+builder.Logging.AddLog4Net();
+
 
 // Add services to the container.
 
@@ -6,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
