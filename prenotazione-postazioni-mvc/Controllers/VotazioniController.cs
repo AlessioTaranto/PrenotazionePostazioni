54ab29660e282916aba2483990a32f7d6b2679b6
Schyerke <stefanohu926@hotmail.com>
2022-07-18 09:44:45 +0200

    merge fixed

--- prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
+++ prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
@@ -1,12 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace prenotazione_postazioni_mvc.Controllers
-{
     public class VotazioniController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
