5b8ac01d78b91f098cc1426d7182721588b4a03f
DESKTOP-B8G3449\redeg <redegalli03@gmail.com>
2022-07-19 13:14:44 +0200

    Inizio controller Capienza (Impostazioni)

--- prenotazione-postazioni-mvc/Controllers/ImpostazioniController.cs
+++ prenotazione-postazioni-mvc/Controllers/ImpostazioniController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prenotazione_postazioni_mvc.Models;
 
 namespace prenotazione_postazioni_mvc.Controllers
 {
@@ -6,7 +7,27 @@ namespace prenotazione_postazioni_mvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            if (ViewModel == null)
+                ViewModel = new ImpostazioniViewModel(
+                    new CapienzaImpostazioniViewModel(), 
+                    new FestivitaImpostazioniViewModel(), 
+                    new PresenzeImpostazioniViewModel()
+                );
+
+            return View(ViewModel);
         }
+
+        public static ImpostazioniViewModel ViewModel;
+
+        [HttpPost]
+        [ActionName("SelectRoom")]
+        public IActionResult SelectRoom(string stanza)
+        {
+            ViewModel.CapienzaViewModel.Stanza = stanza;
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
--- prenotazione-postazioni-mvc/Models/ImpostazioniViewModel.cs
+++ prenotazione-postazioni-mvc/Models/ImpostazioniViewModel.cs
@@ -2,5 +2,16 @@
 {
     public class ImpostazioniViewModel
     {
+
+        public CapienzaImpostazioniViewModel CapienzaViewModel { get; set; }
+        public FestivitaImpostazioniViewModel FestivitaViewModel { get; set; }
+        public PresenzeImpostazioniViewModel PresenzeViewModel { get; set; }
+
+        public ImpostazioniViewModel(CapienzaImpostazioniViewModel capienzaViewModel, FestivitaImpostazioniViewModel festivitaViewModel, PresenzeImpostazioniViewModel presenzeViewModel)
+        {
+            CapienzaViewModel = capienzaViewModel;
+            FestivitaViewModel = festivitaViewModel;
+            PresenzeViewModel = presenzeViewModel;
+        }
     }
 }
