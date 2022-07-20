458a0b509ab83301187a66ca991b888ed621b0e0
LTP1002\User <arianna.bustone@gmail.com>
2022-07-20 11:34:18 +0200

    controller votazioni (incompleto)

--- prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
+++ prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using prenotazione_postazioni_mvc.Models;
 
 namespace prenotazione_postazioni_mvc.Controllers
 {
     public class VotazioniController : Controller
     {
+
+        public static VotazioniViewModel ViewModel { get; set; }
+
         public IActionResult Index()
         {
-            return View();
+            if(ViewModel == null)
+                ViewModel = new VotazioniViewModel();
+            return View(ViewModel);
+        }
+
+        [HttpPost]
+        [ActionName("VoteUser")]
+        public IActionResult VoteUser(bool? voto, int i)
+        {
+            try
+            {
+                ViewModel.Votazioni[i] = voto;
+            } catch(Exception)
+            {
+                ViewModel.Votazioni.Add(voto);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
--- prenotazione-postazioni-mvc/Models/VotazioniViewModel.cs
+++ prenotazione-postazioni-mvc/Models/VotazioniViewModel.cs
@@ -2,6 +2,22 @@
 {
     public class VotazioniViewModel
     {
-        
+        public List<bool?> Votazioni { get; set; }
+
+        public VotazioniViewModel(List<bool?> votazioni)
+        {
+            Votazioni = votazioni;
+        }
+
+        public VotazioniViewModel()
+        {
+            Votazioni = new List<bool?>();
+        }
+
+        public string VotazioniJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(Votazioni);
+        }
+
     }
 }
