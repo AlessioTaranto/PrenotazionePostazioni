bf6554776f619d65f01dc60a7a5bcff55f0fa73e
manuel mauro <manuelmauro04@gmail.com>
2022-07-20 09:33:51 +0200

    Cambiato Model Festa

--- prenotazioni-postazioni-api/Repositories/FestaRepository.cs
+++ prenotazioni-postazioni-api/Repositories/FestaRepository.cs
@@ -44,7 +44,7 @@ namespace prenotazioni_postazioni_api.Repositories
         /// <param name="festa">la festa da salvare</param>
         internal void Save(Festa festa)
         {
-            string query = $"INSERT INTO Festa (giorno, descrizione) VALUES ('{festa.Date.ToString("yyyy-MM-dd hh:mm:ss:fff")}', '{festa.Desc})';";
+            string query = $"INSERT INTO Festa (giorno, descrizione) VALUES ('{festa.Giorno.ToString("yyyy-MM-dd hh:mm:ss:fff")}', '{festa.Descrizione})';";
             DatabaseManager<object>.GetInstance().MakeQueryNoResult(query);
         }
     }
