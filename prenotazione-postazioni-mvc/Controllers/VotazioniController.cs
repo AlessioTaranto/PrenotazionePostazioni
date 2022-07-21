99160045e8499fd46737affe137e75d968434b7a
manuel mauro <manuelmauro04@gmail.com>
2022-07-21 17:58:39 +0200

    Added MakeVotoToUtente

--- prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
+++ prenotazione-postazioni-mvc/Controllers/VotazioniController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using prenotazione_postazioni_libs.Dto;
+using prenotazione_postazioni_libs.Models;
+using prenotazione_postazioni_mvc.HttpServices;
 using prenotazione_postazioni_mvc.Models;
 
 namespace prenotazione_postazioni_mvc.Controllers
@@ -8,6 +11,12 @@ namespace prenotazione_postazioni_mvc.Controllers
     {
 
         public static VotazioniViewModel ViewModel { get; set; }
+        public readonly VotoHttpService _votoHttpService;
+
+        public VotazioniController(VotoHttpService votoHttpService)
+        {
+            _votoHttpService = votoHttpService;
+        }
 
         public IActionResult Index()
         {
@@ -18,27 +27,37 @@ namespace prenotazione_postazioni_mvc.Controllers
 
         [HttpPost]
         [ActionName("VoteUser")]
-        public IActionResult VoteUser(int voto, int i)
+        public IActionResult VoteUser(int voto, Utente UtenteVotato, Utente Utente)
         {
-            try
+            /*try
             {
                 ViewModel.Votazioni[i] = voto;
             } catch(Exception)
             {
                 ///controllo se sono presenti elementi prima della posizione in cui si vuole inserire il voto
-                if (i != ViewModel.Votazioni.Count)
+                if (idUtenteVotato != ViewModel.Votazioni.Count)
                 {
                     ///il numero di cicli rappresenta il numero di posizioni da riempire (con un voto nullo)
-                    int cicli = i - ViewModel.Votazioni.Count;
+                    int cicli = idUtenteVotato - ViewModel.Votazioni.Count;
                     for (int j = 0; j < cicli; j++)
                     {
                         ViewModel.Votazioni.Add(0);
                     }
                 }
                 ViewModel.Votazioni.Add(voto);
+            }*/
+            if(voto == 0)
+            {
+
+            }else if(voto == 1)
+            {
+                _votoHttpService.OnMakeVoto(new VotoDto(Utente, UtenteVotato, true));
+            }else if(voto == -1)
+            {
+                _votoHttpService.OnMakeVoto(new VotoDto(Utente, UtenteVotato, false));
             }
 
-            return RedirectToAction("Index");
+            return Ok("Votazionr effettuata");
         }
     }
 }
