0b221b8bd71af05c0c45e74bc98f490ab7d8e7af
DESKTOP-B8G3449\redeg <redegalli03@gmail.com>
2022-07-22 13:44:48 +0200

    Aggiunto FestaHttpService

---
+++ prenotazione-postazioni-mvc/HttpServices/FestaHttpService.cs
@@ -0,0 +1,24 @@
+﻿namespace prenotazione_postazioni_mvc.HttpServices
+{
+    public class FestaHttpService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FestaHttpService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<HttpResponseMessage> getCovidMode()
+        {
+            var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazione-Capienza");
+
+            var httpResponseMessage =
+                await httpClient.GetAsync($"https://localhost:7126/api/impostazioni/getImpostazioneEmergenza");
+
+            return httpResponseMessage;
+        }
+
+    }
+
+}
