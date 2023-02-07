2557061bcb20a45d2c98d028beedb311f3973396
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-02-07 02:25:36 +0100

    Aggiunto utente fittizio, fixare errore 400 nella creazione

--- prenotazione-postazioni-mvc/Controllers/HomeController.cs
+++ prenotazione-postazioni-mvc/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using prenotazione_postazioni_mvc.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace prenotazione_postazioni_mvc.Controllers;
 
@@ -143,4 +147,38 @@ public class HomeController : Controller
     {
         throw new NotImplementedException();
     }
+
+    //TESTING
+    
+    /*[ActionName("Login")]
+    public async Task Login()
+    {
+        await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
+        {
+            RedirectUri = Url.Action("GoogleResponse")
+        });
+    }
+
+    [ActionName("GoogleResponse")]
+    public async Task<IActionResult> GoogleResponse()
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var claims = result.Principal.Identities
+            .FirstOrDefault().Claims.Select(claim => new
+            {
+                claim.Issuer,
+                claim.OriginalIssuer,
+                claim.Type,
+                claim.Value
+            });
+        return Json(claims);
+    }
+
+    [Authorize]
+    [ActionName("Logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return RedirectToAction("Index");
+    }*/
 }
