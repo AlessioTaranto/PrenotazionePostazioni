ab2a1d7413e6a720bc654462ce72fe74fe02cecf
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-02-08 20:51:23 +0100

    Modificato parametro Ruolo in "idRuolo"
    (7/02/2023)

--- prenotazione-postazioni-libs/Dto/UtenteDto.cs
+++ prenotazione-postazioni-libs/Dto/UtenteDto.cs
@@ -14,14 +14,14 @@ namespace prenotazione_postazioni_libs.Dto
         public string Image { get; set; }
         //Base 64x converter (https://stackoverflow.com/questions/69303512/converting-an-sql-Image-to-base64-in-c-sharp)
         public string Email { get; set; }
-        public Ruolo Ruolo { get; set; }
-        public UtenteDto(string nome, string cognome, string image, string email, Ruolo ruolo)
+        public int IdRuolo { get; set; }
+        public UtenteDto(string nome, string cognome, string image, string email, int idRuolo)
         {
             this.Nome = nome;
             this.Cognome = cognome;
             this.Image = image;
             this.Email = email;
-            this.Ruolo = ruolo;
+            this.IdRuolo = idRuolo;
         }
 
    
