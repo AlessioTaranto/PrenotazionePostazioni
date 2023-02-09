fdee8efe592a17597fa2c22580f2ceabb9ccde87
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-02-09 11:52:23 +0100

    Creazione PrenotazioneHttpService

---
+++ prenotazione-postazioni-mvc/HttpServices/PrenotazioneHttpSerivice.cs
@@ -0,0 +1,23 @@
+﻿namespace prenotazione_postazioni_mvc.HttpServices
+{
+    public class PrenotazioneHttpSerivice
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PrenotazioneHttpSerivice(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<HttpResponseMessage> OnGetPrenotazioneById(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazioni-Prenotazioni");
+
+            var httpResponseMessage = await httpClient.GetAsync("https://localhost:7126/api/prenotazioni/getPrenotazioneById?idPrenotazione={id}");
+
+            return httpResponseMessage;
+        }
+
+    }
+}
