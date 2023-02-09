8efeda0178f90c21c8ea17322981aefaca8b2f1d
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-02-09 12:01:57 +0100

    Implementate tutte le funzioni API in PrenotazioneHttpService

--- prenotazione-postazioni-mvc/HttpServices/PrenotazioneHttpSerivice.cs
+++ prenotazione-postazioni-mvc/HttpServices/PrenotazioneHttpSerivice.cs
@@ -14,7 +14,43 @@
         {
             var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazioni-Prenotazioni");
 
-            var httpResponseMessage = await httpClient.GetAsync("https://localhost:7126/api/prenotazioni/getPrenotazioneById?idPrenotazione={id}");
+            var httpResponseMessage = await httpClient.GetAsync($"https://localhost:7126/api/prenotazioni/getPrenotazioneById?idPrenotazione={id}");
+
+            return httpResponseMessage;
+        }
+
+        public async Task<HttpResponseMessage> OnGetAllPrenotazioni()
+        {
+            var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazioni-Prenotazioni");
+
+            var httpResponseMessage = await httpClient.GetAsync($"https://localhost:7126/api/prenotazioni/getAllPrenotazioni");
+
+            return httpResponseMessage;
+        }
+
+        public async Task<HttpResponseMessage> OnGetPrenotazioneByStanza(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazioni-Prenotazioni");
+
+            var httpResponseMessage = await httpClient.GetAsync($"https://localhost:7126/api/prenotazioni/getPrenotazioniByStanza?idStanza={id}");
+
+            return httpResponseMessage;
+        }
+
+        public async Task<HttpResponseMessage> OnGetPrenotazioneByUtente(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazioni-Prenotazioni");
+
+            var httpResponseMessage = await httpClient.GetAsync($"https://localhost:7126/api/prenotazioni/getPrenotazioniByUtente?idUtente={id}");
+
+            return httpResponseMessage;
+        }
+
+        public async Task<HttpResponseMessage> OnGetPrenotazioneByDate(int idStanza, DateTime start, DateTime end)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PrenotazionePostazioni-Prenotazioni");
+
+            var httpResponseMessage = await httpClient.GetAsync($"https://localhost:7126/api/prenotazioni/getPrenotazioniByDate?idStanza={idStanza}&startDateYear={start.Year}&startDateMonth={start.Month}&startDateDay={start.Day}&endDateYear={end.Year}&endDateMonth={end.Month}&endDateDay={end.Day}");
 
             return httpResponseMessage;
         }
