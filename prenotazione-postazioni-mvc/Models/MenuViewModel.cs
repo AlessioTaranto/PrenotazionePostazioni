917e87b32659095c7ca93f17e652b501dd8a13a6
LTP090\Stagista-4 <miccijhonandrea@gmail.com>
2023-07-11 12:32:45 +0200

    UI ordinazione

---
+++ prenotazione-postazioni-mvc/Models/MenuViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace prenotazione_postazioni_mvc.Models
+{
+    public class MenuViewModel
+    {
+        public DateTime Date { get; set; }
+        public string Scelta { get; set; } 
+        public string UrlMenu { get; set; }
+
+        public MenuViewModel()
+        {
+            UrlMenu = "https://marketplace.canva.com/EAD0UMPtOv4/1/0/1236w/canva-oro-tenue-elegante-matrimonio-menu-FQIyCfgp1aY.jpg";
+            Date = DateTime.Now;
+        }
+        public MenuViewModel(string scelta) { Scelta = scelta; }
+       
+
+        public string GetDate() { return Date.ToString("dd/MM"); }
+
+
+        public string GetUrlMenu()
+        {
+            //richiesta url menu
+            return UrlMenu;
+        }
+
+    }
+}
